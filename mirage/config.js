85c5c70f80a706567656438628a18d9ad0692c77
jetpackpony <jetpackpony@gmail.com>
2016-10-15 16:12:23 +0300

    Remove hard coded filter values of the cities

--- mirage/config.js
+++ mirage/config.js
@@ -1,43 +1,48 @@
 export default function() {
   this.namespace = '/api';
 
+  let routes = [
+    {
+      id: "1",
+      type: "routes",
+      attributes: {
+        title: "Bus Siam Tour",
+        description: "A very fast bus also so cheap",
+        price: "300-400 RUB",
+        from_city: "Bangkok, Thailand",
+        to_city: "Phuket, Thailand"
+      }
+    }, {
+      id: "2",
+      type: "routes",
+      attributes: {
+        title: "Ferry Siam Tour",
+        description: "A very fast bus also so cheap",
+        price: "3300-5400 RUB",
+        from_city: "Bangkok, Thailand",
+        to_city: "Phuket, Thailand"
+      }
+    }, {
+      id: "3",
+      type: "routes",
+      attributes: {
+        title: "Train Ololo Tour",
+        description: "A very fast bus also so cheap",
+        price: "300-400 RUB",
+        from_city: "Bangkok, Thailand",
+        to_city: "Phuket, Thailand"
+      }
+    }
+  ];
+
   this.get('/routes', function(schema, request) {
-    if (request.queryParams.from === "Bangkok, Thailand" &&
-        request.queryParams.to === "Phuket, Thailand") {
+    if (request.queryParams.from !== undefined &&
+        request.queryParams.to !== undefined) {
       return {
-        data: [
-          {
-            id: "1",
-            type: "routes",
-            attributes: {
-              title: "Bus Siam Tour",
-              description: "A very fast bus also so cheap",
-              price: "300-400 RUB",
-              from_city: "Bangkok, Thailand",
-              to_city: "Phuket, Thailand"
-            }
-          }, {
-            id: "2",
-            type: "routes",
-            attributes: {
-              title: "Ferry Siam Tour",
-              description: "A very fast bus also so cheap",
-              price: "3300-5400 RUB",
-              from_city: "Bangkok, Thailand",
-              to_city: "Phuket, Thailand"
-            }
-          }, {
-            id: "3",
-            type: "routes",
-            attributes: {
-              title: "Train Ololo Tour",
-              description: "A very fast bus also so cheap",
-              price: "300-400 RUB",
-              from_city: "Bangkok, Thailand",
-              to_city: "Phuket, Thailand"
-            }
-          }
-        ]
+        data: routes.filter(function(x) {
+          return x.attributes.from_city === request.queryParams.from &&
+            x.attributes.to_city === request.queryParams.to;
+        })
       };
     } else {
       return { data: [] };
