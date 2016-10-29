ca8ac20299d5a69f0ca58fb23456515067e3ef8b
jetpackpony <jetpackpony@gmail.com>
2016-10-30 00:43:09 +0300

    Stub g-maps component in acceptance tests

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -4,13 +4,29 @@ import { module } from 'qunit';
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
+import Resolver from '../../resolver';
 
 const { RSVP: { Promise } } = Ember;
+
+let MockResolver = Resolver.extend({
+  resolveOther: function(parsedName) {
+    if (parsedName.fullName === "component:g-maps") {
+      return undefined;
+    } else {
+      return this._super(parsedName);
+    }
+  }
+});
 
 export default function(name, options = {}) {
   module(name, {
     beforeEach() {
-      this.application = startApp();
+
+      this.application = startApp({
+        Resolver: MockResolver
+      });
+      this.application.unregister('component:g-maps');
+      this.application.register('component:g-maps', Ember.Component.extend());
 
       if (options.beforeEach) {
         return options.beforeEach.apply(this, arguments);
