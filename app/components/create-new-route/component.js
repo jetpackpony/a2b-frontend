942c10a8865a7c70113840f432e8347486f14862
jetpackpony <jetpackpony@gmail.com>
2016-11-06 14:28:38 +0300

    Integrate initial parts of add form

--- app/components/create-new-route/component.js
+++ app/components/create-new-route/component.js
@@ -1,10 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  travelCountry: "",
+  countries: ["Vietnam","Thailand","Cambodia"],
+  currentStep: 2,
   actions: {
     createRoute(route) {
       // Bubble the event up
       this.attrs.createRoute(route);
+    },
+    next() {
+      console.log("route: ", this.get('newRoute'));
+      this.incrementProperty('currentStep');
+    },
+    submit() {
+      console.log("complete: ", this.get('newRoute'));
     }
   }
 });
