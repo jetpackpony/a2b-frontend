8af95b3e0c0db9ff4ba8f91a8d7f39d30c04bb80
jetpackpony <jetpackpony@gmail.com>
2016-11-06 14:27:43 +0300

    Simplify choose location component

--- app/components/create-new-route-choose-location/component.js
+++ app/components/create-new-route-choose-location/component.js
@@ -1,18 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  _processResponse(obj) {
-    return `${obj.address_components.find((item) => item.types.includes("locality")).long_name}, ${obj.address_components.find((item) => item.types.includes("country")).long_name}`;
-  },
-  resCity: "",
-  resAddress: "",
-  resCoords: "",
   actions: {
     cityChanged(obj) {
       if (obj.address_components) {
-        this.set('resCity', this._processResponse(obj));
-        console.log(this.get('resCity'));
+        console.log('city changed: ', obj);
       }
+    },
+    next() {
+      this.attrs.next();
     }
   }
 });
