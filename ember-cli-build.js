3b0c13e4314bb796c70bc4d528e54688839c5ba0
jetpackpony <jetpackpony@gmail.com>
2016-11-07 17:24:35 +0300

    Remove jquery from build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,7 +20,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import(app.bowerDirectory + '/jquery/dist/jquery.min.js');
   app.import(app.bowerDirectory + '/tether/dist/js/tether.min.js');
   app.import(app.bowerDirectory + '/tether/dist/css/tether.min.css');
   app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
