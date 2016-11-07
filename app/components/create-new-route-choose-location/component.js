7caadcd6ece6996ba147a1ceaa24796135d0784c
jetpackpony <jetpackpony@gmail.com>
2016-11-07 19:38:50 +0300

    Add country select dropdown

--- app/components/create-new-route-choose-location/component.js
+++ app/components/create-new-route-choose-location/component.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  countryRestriction: null,
+  countries: [
+    { text: "Vietnam", value: "vn" },
+    { text: "Cambodia", value: "kh" },
+    { text: "Laos", value: "la" },
+    { text: "Myanmar", value: "mm" },
+    { text: "Thailand", value: "th" },
+    { text: "Malaysia", value: "my" },
+    { text: "Brunei", value: "bn" },
+    { text: "East Timor", value: "tl" },
+    { text: "Indonesia", value: "id" },
+    { text: "Singapore", value: "sg" },
+    { text: "Philippines", value: "ph" }
+  ],
   actions: {
+    countryChanged() {
+      this.set('countryRestriction', { country: event.target.value });
+    },
     cityChanged(obj) {
       if (obj.address_components) {
         console.log('city changed: ', obj);
