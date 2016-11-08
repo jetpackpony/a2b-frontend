0febbc641d91359d664b5a064210ce529ff2bfed
jetpackpony <jetpackpony@gmail.com>
2016-11-08 22:07:56 +0300

    Refactor map component to use computed properties

--- app/components/itineraries-map.js
+++ app/components/itineraries-map.js
@@ -5,44 +5,30 @@ export default Ember.Component.extend({
   lat: 15,
   lng: 100,
   zoom: 4,
-  init() {
-    this._super(...arguments);
-    this._redrawLines();
-  },
-  onItineraryChange: Ember.observer('selectedItinerary', function() {
-    this._redrawLines();
-    this._boundsToSelectedItinerary();
+  bounds: Ember.computed('itineraries', function() {
+    let bounds = new google.maps.LatLngBounds();
+    this.get('itineraries').forEach((iti) => {
+      iti.get('routes').map((route) => {
+        let from = route.get('fromCoords').split(', ').map(parseFloat);
+        let to = route.get('toCoords').split(', ').map(parseFloat);
+        bounds.extend(new google.maps.LatLng(from[0], from[1]));
+        bounds.extend(new google.maps.LatLng(to[0], to[1]));
+      })
+    });
+    return bounds;
+  }),
+  polylines: Ember.computed('selectedItinerary', function() {
+    return this.get('selectedItinerary').get('routes').map((item) => {
+      let from = item.get('fromCoords').split(', ').map(parseFloat);
+      let to = item.get('toCoords').split(', ').map(parseFloat);
+      return this.get('_makeLine')(from, to);
+    });
   }),
   actions: {
     mapLoaded() {
-      this._boundsToSelectedItinerary();
+      this.get('gMap').maps.select('my-map').
+        map.fitBounds(this.get('bounds'));
     }
-  },
-  _boundsToSelectedItinerary() {
-    let coords = [];
-    this.get('selectedItinerary').get('routes').map((item) => {
-      let from = item.get('fromCoords').split(', ').map(parseFloat);
-      let to = item.get('toCoords').split(', ').map(parseFloat);
-      coords.push({ lat: from[0], lng: from[1] });
-      coords.push({ lat: to[0], lng: to[1] });
-    });
-    this.get('gMap').maps.select('my-map').
-      map.fitBounds(this._createBoundsFor(coords));
-  },
-  _createBoundsFor(coords) {
-    let bounds = new google.maps.LatLngBounds();
-    coords.forEach((m) => {
-      bounds.extend(new google.maps.LatLng(m.lat, m.lng));
-    });
-    return bounds;
-  },
-  _redrawLines() {
-    this.set('polylines', Ember.A([]));
-    this.get('selectedItinerary').get('routes').map((item) => {
-      let from = item.get('fromCoords').split(', ').map(parseFloat);
-      let to = item.get('toCoords').split(', ').map(parseFloat);
-      this.get('polylines').pushObject(this.get('_makeLine')(from, to));
-    });
   },
   _makeLine(from, to) {
     return {
@@ -63,6 +49,5 @@ export default Ember.Component.extend({
           visible: true,
           zIndex: 999
         };
-  },
-  polylines: Ember.A([])
+  }
 });
