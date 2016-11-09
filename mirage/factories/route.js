2148ac8a6e7dc799219a8f00baec23881baa0f5b
jetpackpony <jetpackpony@gmail.com>
2016-11-09 22:30:11 +0300

    Change factories to use faker porperly

--- mirage/factories/route.js
+++ mirage/factories/route.js
@@ -1,19 +1,31 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  fromAddress: faker.address.streetName(),
-  fromCity: faker.fake("{{address.city}}, {{address.country}}"),
-  fromCoords: faker.fake("{{address.latitude}}, {{address.longitude}}"),
+  fromAddress: faker.address.streetName,
+  fromCity() {
+    return faker.fake("{{address.city}}, {{address.country}}");
+  },
+  fromCoords() {
+    return faker.fake("{{address.city}}, {{address.country}}");
+  },
 
-  toAddress: faker.address.streetName(),
-  toCity: faker.fake("{{address.city}}, {{address.country}}"),
-  toCoords: faker.fake("{{address.latitude}}, {{address.longitude}}"),
+  toAddress: faker.address.streetName,
+  toCity() {
+    return faker.fake("{{address.city}}, {{address.country}}");
+  },
+  toCoords() {
+    return faker.fake("{{address.city}}, {{address.country}}");
+  },
 
-  transportType: faker.list.random('bus', 'train', 'ferry', 'choppah'),
-  organization: faker.company.companyName(),
-  description: faker.lorem.paragraph(),
+  transportType: faker.list.cycle('bus', 'train', 'ferry'),
+  organization: faker.list.cycle('Giant Express', 'AsiaLines', 'WaterCamb'),
+  description: "Such comfort! Much VIP! WOW!",
 
-  duration: faker.random.number(),
-  price: faker.random.number(),
-  timetable: faker.list.random("7:00 AM", "5:00 PM", "12:00 PM", "12:02 AM")
+  duration() {
+    return faker.random.number(24);
+  },
+  price() {
+    return faker.random.number(150);
+  },
+  timetable: faker.list.random("7:00 AM, 8:00 PM", "5:00 PM", "12:00 PM", "12:02 AM")
 });
