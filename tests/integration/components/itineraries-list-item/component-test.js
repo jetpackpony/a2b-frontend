89fcf53cd12539b51bb9ff03abf76c3f09df7031
jetpackpony <jetpackpony@gmail.com>
2016-11-09 16:41:31 +0300

    Add itineraries list item; Handle it's events outside of it

--- tests/integration/components/itineraries-list-item/component-test.js
+++ tests/integration/components/itineraries-list-item/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('itinerary-details', 'Integration | Component | itinerary details', {
+moduleForComponent('itineraries-list-item', 'Integration | Component | itineraries list item', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{itinerary-details}}`);
+  this.render(hbs`{{itineraries-list-item}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#itinerary-details}}
+    {{#itineraries-list-item}}
       template block text
-    {{/itinerary-details}}
+    {{/itineraries-list-item}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
