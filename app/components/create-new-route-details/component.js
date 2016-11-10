bd4cfd59580a7ea35b958450ea8d32b16ab7fd2d
jetpackpony <jetpackpony@gmail.com>
2016-11-10 20:59:57 +0300

    Show submit button only if the form is filled

--- app/components/create-new-route-details/component.js
+++ app/components/create-new-route-details/component.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newRoute: null,
+  showSubmitButton: Ember.computed('newRoute.{transportType,organization,duration,price,timetable}', function() {
+    let route = this.get('newRoute');
+    if (route.get('transportType') && route.get('organization')
+        && route.get('duration') && route.get('price')
+        && route.get('timetable')) {
+      return true;
+    }
+    return false;
+  }),
   actions: {
     submit() {
       this.attrs.submit();
