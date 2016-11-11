31dbcc3e4e39dd54874c0969eb84b75c3ab257dc
jetpackpony <jetpackpony@gmail.com>
2016-11-11 11:57:06 +0300

    Move duration and price to the right in itinerary list

--- app/components/itineraries-list-item/component.js
+++ app/components/itineraries-list-item/component.js
@@ -5,6 +5,14 @@ export default Ember.Component.extend({
   itinerary: null,
   routeHovered: null,
   routeOpened: null,
+  duration: Ember.computed('itinerary.duration', function() {
+    let duration = this.get('itinerary.duration');
+    if (duration % 1 === 0) {
+      return `${duration - (duration % 1)} hours`;
+    } else {
+      return `${duration - (duration % 1)}h ${(duration % 1) * 60}m`
+    }
+  }),
 
   actions: {
     routeHovered(route) {
