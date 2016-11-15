664826d4fbf7ef7c89081ee9d6a6b29f9657db70
jetpackpony <jetpackpony@gmail.com>
2016-11-15 21:46:06 +0300

    Add a login form

---
+++ app/login/route.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend({
+});
--- app/router.js
+++ app/router.js
@@ -13,6 +13,7 @@ Router.map(function() {
     this.route('new');
   });
   this.route('about');
+  this.route('login');
 });
 
 export default Router;
