c52f54ca344e073b7f3022914766507bc9ae5424
jetpackpony <jetpackpony@gmail.com>
2016-11-16 16:33:57 +0300

    Add console.logs to all api calls in mirage

--- mirage/config.js
+++ mirage/config.js
@@ -2,6 +2,7 @@ export default function() {
   this.namespace = '/api';
 
   this.get('/itineraries', function(schema, request) {
+    console.log(request);
     let from = request.queryParams['filter[from]'];
     let to = request.queryParams['filter[to]'];
     if (from !== undefined && to !== undefined) {
@@ -12,11 +13,13 @@ export default function() {
   });
 
   this.post('/routes', function(schema, request) {
+    console.log(request);
     const attrs = JSON.parse(request.requestBody);
     return schema.routes.create(attrs);
   });
 
   this.post('/session/create', function(schema, request) {
+    console.log(request);
     return {
       "access_token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJlbWFpbCI6InRlc3RtZUBnbWFpbC5jb20ifQ.36IvcD5fisgSmPxGDbtuAZF3nSeBxX2xEy4vnQTa18E",
       "token_type":"JWT",
@@ -25,6 +28,7 @@ export default function() {
   });
 
   this.post('/users', function(schema, request) {
+    console.log(request);
     const attrs = JSON.parse(request.requestBody);
     return { data: [] };
   });
