ee1196b20b86273e6231086c46e4ab4714baa483
jetpackpony <jetpackpony@gmail.com>
2016-11-18 23:32:34 +0300

    Move all map communication into create-new-route-map component

--- app/components/create-new-route/component.js
+++ app/components/create-new-route/component.js
@@ -1,20 +1,9 @@
 import Ember from 'ember';
 
-let asia = {
-  lat: 15,
-  lng: 100,
-  zoom: 4
-};
-
 export default Ember.Component.extend({
   classNames: ['row', 'bottom-split', 'add-route-form'],
   complete: false,
   errorMessage: null,
-  markers: Ember.A([]),
-  lines: Ember.A([]),
-  bounds: Ember.A([]),
-  center: [asia.lat, asia.lng],
-  zoom: asia.zoom,
   locationsNumber: 2,
   currentStep: 1,
   children: Ember.A([]),
@@ -35,38 +24,6 @@ export default Ember.Component.extend({
   },
   locationChanged: Ember.observer('locations.@each.{country,city,address,comment}', 'stepTwoLocation', function() {
     let locs = this.get('locations');
-    let addrs = locs.mapBy('address').compact();
-
-    // if more than one address, draw lines between them
-    this.set('lines', Ember.A([]));
-    if (addrs.length > 1) {
-      let prev = null;
-      addrs.forEach((addr) => {
-        if (prev) {
-          this.get('lines').pushObject({
-            id: `${prev.formatted_address}-${addr.formatted_address}`,
-            from: [prev.geometry.location.lat(), prev.geometry.location.lng()],
-            to: [addr.geometry.location.lat(), addr.geometry.location.lng()],
-            style: 'selected'
-          });
-        }
-        prev = addr;
-      });
-    }
-
-    // draw markers for every set address
-    this.set('markers', Ember.A([]));
-    addrs.forEach((addr) => {
-      this.get('markers').pushObject({
-        id: `${addr.formatted_address}`,
-        coords: [addr.geometry.location.lat(), addr.geometry.location.lng()],
-        style: 'selected',
-        title: addr.formatted_address
-      });
-    });
-
-    // Focus the map
-    this._focusMap();
 
     // Add the info to the model
     this.set('newRoute.fromCity', this._getFormattedAddr(locs[0], "city"));
@@ -79,9 +36,6 @@ export default Ember.Component.extend({
     this.set('newRoute.toCoords', this._getCoords(locs[1].address).join(', '));
     this.set('newRoute.toComment', locs[1].comment);
   }),
-  currentStepChanged: Ember.observer('currentStep', function() {
-    this._focusMap();
-  }),
   _getCoords(addr) {
     if (addr && addr.geometry.location) {
       return [addr.geometry.location.lat(), addr.geometry.location.lng()];
@@ -93,53 +47,6 @@ export default Ember.Component.extend({
       return addr[prop].formatted_address;
     }
     return null;
-  },
-  _focusMap() {
-    let step = this.get('currentStep');
-    let locs = this.get('locations');
-    // if step is more than number of addresses, zoom out to whole picture
-    if (step > this.get('locationsNumber')) {
-      this.set('bounds', locs.map((loc) => {
-          let place = (loc.address || loc.city || loc.country);
-          if (place) {
-            return [place.geometry.location.lat(), place.geometry.location.lng()];
-          } else {
-            return [asia.lat, asia.lng];
-          }
-        })
-      );
-    } else {
-      // else focus on the current step-point
-      let loc = locs[step - 1];
-      let place;
-
-      // If address is there, focus on it's point
-      if (loc.address) {
-        let point = loc.address.geometry.location;
-        this.set('center', [point.lat(), point.lng()]);
-        // If the zoom is too far, come closer
-        if (this.get('zoom') < 12) {
-          this.set('zoom', 12);
-        }
-      } else if (place = loc.city || loc.country) {
-        let bounds = place.geometry.viewport;
-        this.set('bounds', Ember.A([
-          [bounds.getNorthEast().lat(), bounds.getNorthEast().lng()],
-          [bounds.getSouthWest().lat(), bounds.getSouthWest().lng()]
-        ]));
-      } else {
-        this.set('center', [asia.lat, asia.lng]);
-        this.set('zoom', asia.zoom);
-      }
-    }
-  },
-  _handleClick(addr) {
-    let step = this.get('currentStep');
-    let locs = this.get('locations');
-    let loc = locs[step - 1];
-    if (loc.country && loc.city) {
-      Ember.set(loc, 'address', addr);
-    }
   },
 
   didInsertElement() {
@@ -173,11 +80,6 @@ export default Ember.Component.extend({
     },
     resetForm() {
       this.set('locations', this.resetLocations());
-      this.set('markers', Ember.A([]));
-      this.set('lines', Ember.A([]));
-      this.set('bounds', Ember.A([]));
-      this.set('center', [asia.lat, asia.lng]);
-      this.set('zoom', asia.zoom);
       this.get('children').forEach((view) => {
         return view.ref.reset();
       });
@@ -189,18 +91,6 @@ export default Ember.Component.extend({
     },
     registerChild(id, child) {
       this.get('children').pushObject({ id, ref: child });
-    },
-    mapClicked(geocodes, point) {
-      let addr = geocodes.find((item) => item.types.includes('route'))
-        || geocodes.find((item) => item.types.includes('street_address'));
-      if (addr) {
-        addr.geometry.location.lat = function() { return point[0]; }
-        addr.geometry.location.lng = function() { return point[1]; }
-        this._handleClick(addr);
-      } else {
-        console.log("Couldn't find geocode", geocodes);
-      }
-      //geocodes.find((item) => item.types.includes('locality'))
     }
   }
 });
