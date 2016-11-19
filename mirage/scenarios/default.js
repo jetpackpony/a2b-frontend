da30ad6f33eeacba7f69ef81cfa3b2d6d57b90c2
jetpackpony <jetpackpony@gmail.com>
2016-11-19 18:03:42 +0300

    Improve random test data

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -10,29 +10,30 @@ const maxRoutes = 3;
 
 export default function(server) {
 
-  let from = { fromCity: "Bangkok, Thailand", fromCoords: "13.7563, 100.5018" };
-  let to = { toCity: "Phuket, Thailand", toCoords: "7.8804, 98.3922" };
+  let from = { city: "Bangkok, Thailand", coords: "13.7563, 100.5018" };
+  let to = { city: "Phuket, Thailand", coords: "7.8804, 98.3922" };
 
   times(maxItineraries)(() => {
     let itinerary = server.create('itinerary');
-    let routesNumber = Math.floor(Math.random() * (maxRoutes + 1)) + 1;
+    let routesNumber = Math.floor(Math.random() * (maxRoutes - 1)) + 1;
     let routes = [];
 
     times(routesNumber)(() => {
       let route = null;
       let coords = {};
       if (routes.length === routesNumber - 1) {
-        coords.toCity = to.toCity;
-        coords.toCoords = to.toCoords;
+        coords.toCity = to.city;
+        coords.toCoords = to.coords;
       }
       if (routes.length > 0) {
         let prev = routes[routes.length - 1];
         coords.fromCity = prev.toCity;
         coords.fromCoords = prev.toCoords;
-        route = server.create('route', coords);
       } else {
-        route = server.create('route', from);
+        coords.fromCity = from.city;
+        coords.fromCoords = from.coords;
       }
+      route = server.create('route', coords);
       routes.push(route);
       server.create('itinerary-route', { itinerary, route });
     });
