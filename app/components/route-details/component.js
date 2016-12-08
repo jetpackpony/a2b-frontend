5ef04af6617b047ebf8a6d6c6b427ac759051888
jetpackpony <jetpackpony@gmail.com>
2016-12-08 21:10:00 +0300

    Fixed company desc on route details

--- app/components/route-details/component.js
+++ app/components/route-details/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  description: Ember.computed('route.companyDescription', function() {
+    let desc = this.get('route.companyDescription');
+    if (!desc || desc === 'null')  {
+      return false;
+    }
+    return desc;
+  })
 });
