b98c1f130e0fd44ac4d93c26247ab97fc20441b7
jetpackpony <jetpackpony@gmail.com>
2016-12-08 20:50:31 +0300

    Use sortparams to sort itineraries

--- app/controllers/itineraries.js
+++ app/controllers/itineraries.js
@@ -6,23 +6,8 @@ export default Ember.Controller.extend({
   toCoords: null,
   fromCity: null,
   toCity: null,
-  sortedItineraries: Ember.computed.sort('model', function(a, b) {
-    let aStops = a.get('stops');
-    let bStops = b.get('stops');
-    if (aStops > bStops) {
-      return 1;
-    } else if (aStops < bStops) {
-      return -1;
-    }
-    let aDur = a.get('duration');
-    let bDur = b.get('duration');
-    if (aDur > bDur) {
-      return 1;
-    } else if (aDur < bDur) {
-      return -1;
-    }
-    return 0;
-  }),
+  sortParams: ['stops', 'duration'],
+  sortedItineraries: Ember.computed.sort('model', 'sortParams'),
   formFilled: Ember.computed('fromCoords', 'toCoords', function() {
     return this.get('fromCoords') !== null && this.get('toCoords') !== null &&
       this.get('fromCoords') !== "" && this.get('toCoords') !== "";
