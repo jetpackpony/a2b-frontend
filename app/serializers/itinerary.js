92be9e311be67469d431f91acf8f8b16ca738583
jetpackpony <jetpackpony@gmail.com>
2016-12-08 19:21:10 +0300

    Sort routes inside the itinerary in the correct order

---
+++ app/serializers/itinerary.js
@@ -0,0 +1,22 @@
+import DS from 'ember-data';
+
+const putRoutesInOrder = function(order, items) {
+  let res = order.map((routeId) => {
+    return items.find((route) => route.id === routeId);
+  });
+  return res;
+};
+
+export default DS.JSONAPISerializer.extend({
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    payload.data = payload.data.map((item) => {
+      item.relationships.routes.data = putRoutesInOrder(
+        item.id.split('-'),
+        item.relationships.routes.data
+      );
+      return item;
+    });
+
+    return this._super(...arguments);
+  }
+});
