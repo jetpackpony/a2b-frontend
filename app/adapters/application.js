cca4f4b12c716b60ca09280df7870329f11c9468
jetpackpony <jetpackpony@gmail.com>
2016-12-12 15:33:31 +0300

    Handle all responses with 'errors' in them as errors

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,5 +4,12 @@ import ENV from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:oauth2',
-  host: ENV.a2b.apiEndPoint
+  host: ENV.a2b.apiEndPoint,
+  handleResponse: function(status, headers, payload, req){
+    if(payload.errors){
+      let msg = `Server error occured while processing: '${req.method} ${req.url}'`;
+      return new DS.AdapterError(payload.errors, msg);
+    }
+    return this._super(...arguments);
+  }
 });
