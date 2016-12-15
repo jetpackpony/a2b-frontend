dbfcf0494338223d0d387bd9c66f4ff5a27144f8
jetpackpony <jetpackpony@gmail.com>
2016-12-15 22:42:06 +0300

    Remove suggestions list when no suggestions

--- app/components/location-autocomplete/component.js
+++ app/components/location-autocomplete/component.js
@@ -3,12 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['autocomplete-field'],
   value: '',
-  valueLongEnough: Ember.computed.gt('value.length', 2),
+  hasSuggestions: Ember.computed.gt('suggestions.length', 0),
   suggestions: Ember.A([]),
   actions: {
     handleInput(value) {
       if (value.length > 2)  {
         this.get('filter')(value).then((res) => this.set('suggestions', res));
+      } else {
+        this.set('suggestions', Ember.A([]));
       }
     }
   }
