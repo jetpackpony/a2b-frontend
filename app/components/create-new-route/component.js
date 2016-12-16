3d946805b2e172d993ef3e6751079f358623371b
jetpackpony <jetpackpony@gmail.com>
2016-12-16 23:37:25 +0300

    Add loading geocodes before sending route to the server

--- app/components/create-new-route/component.js
+++ app/components/create-new-route/component.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 import Route from '../../models/route';
+import RSVP from 'rsvp';
 
 export default Ember.Component.extend({
+  _gMap: Ember.inject.service('gMap'),
   classNames: ['row', 'bottom-split', 'add-route-form'],
   complete: false,
   errorMessage: null,
@@ -18,6 +20,7 @@ export default Ember.Component.extend({
         country: null,
         city: null,
         address: null,
+        geocodes: Ember.A([]),
         comment: null
       });
     }
@@ -64,7 +67,8 @@ export default Ember.Component.extend({
       this.$('.carousel').carousel('prev');
     },
     submit() {
-      this.get('createRoute')(
+      this._loadGeocodes().then(() => {
+        this.get('createRoute')(
           this._validateRoute(this.get('newRoute')),
           () => {
             this.send('next');
@@ -74,7 +78,8 @@ export default Ember.Component.extend({
             this.send('next');
             this.set('complete', true);
           }
-      );
+        );
+      });
     },
     resetForm() {
       this.set('locations', this.resetLocations());
@@ -90,6 +95,34 @@ export default Ember.Component.extend({
     registerChild(id, child) {
       this.get('children').pushObject({ id, ref: child });
     }
+  },
+  _loadGeocodes() {
+    let skip = ['street_address', 'route', 'country'];
+    return RSVP.all(this.get('locations').map((loc) => {
+      if (loc.address && loc.address.geometry.location) {
+        return this.get('_gMap').geocode({
+          lat: loc.address.geometry.location.lat(),
+          lng: loc.address.geometry.location.lng(),
+          language: 'en'
+        })
+        .then((geocodes) => {
+          Ember.set(loc, 'geocodes', geocodes.filter((item) => {
+            return !item.types.includes('street_address') && !item.types.includes('country');
+          }).map((item) => {
+            return {
+              "place_id": item.place_id,
+              "name": item.formatted_address
+            }
+          }));
+        });
+      } else {
+        return RSVP.resolve(loc);
+      }
+    })).then(() => {
+      let locs = this.get('locations');
+      this.set(`newRoute.from-locations`, locs[0].geocodes);
+      this.set(`newRoute.to-locations`, locs[1].geocodes);
+    });
   },
   _validateRoute(route) {
     Route.eachAttribute(function(name) {
