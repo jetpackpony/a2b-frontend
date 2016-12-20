6527f3a78774532e9d67bcafd0ac7253e94abfa3
jetpackpony <jetpackpony@gmail.com>
2016-12-20 19:01:36 +0300

    Add 'thanks' message to quick form

--- app/components/create-new-route-short-form/component.js
+++ app/components/create-new-route-short-form/component.js
@@ -1,17 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  scrollContainer: '.left-side',
   children: Ember.A([]),
+  showResults: false,
   init() {
     this._super(...arguments);
     this.get('registerChild')(this);
   },
+  reset() {
+    this.get('children').forEach((view) => {
+      return view.ref.reset();
+    });
+    $(this.get('scrollContainer')).scrollTop(0);
+    this.set('showResults', false);
+  },
   actions: {
-    submit(resolve, reject) {
+    submit() {
       this.get('submit')(
         () => {
+          this.set('showResults', true);
+          $(this.get('scrollContainer')).scrollTop(0);
         },
         (error) => {
+          this.set('showResults', true);
+          $(this.get('scrollContainer')).scrollTop(0);
         }
       );
     },
