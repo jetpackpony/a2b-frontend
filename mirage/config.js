3b07a3ab9a050ab3a6df3164d54f54326d4f94b9
jetpackpony <jetpackpony@gmail.com>
2016-12-21 12:36:10 +0300

    Passthrough non-desctuctive requests

--- mirage/config.js
+++ mirage/config.js
@@ -58,9 +58,9 @@ export default function() {
   });
 
   // Pass it to the actual API
-  /*
   this.passthrough('/itineraries', ['get']);
   this.passthrough('/locations', ['get']);
+  /*
   this.passthrough('/routes', ['post']);
   this.passthrough('/users', ['post']);
   this.passthrough('/session/create', ['post']);
