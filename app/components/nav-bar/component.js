5972c346dbb0aae3b38eaad622bdb98c962579fb
jetpackpony <jetpackpony@gmail.com>
2016-12-21 13:27:56 +0300

    Enable all links  on navbar by default

--- app/components/nav-bar/component.js
+++ app/components/nav-bar/component.js
@@ -1,27 +1,7 @@
 import Ember from 'ember';
 
-Ember.LinkComponent.reopen({
-  disabled: Ember.computed(function(key, value) {
-    if (value !== undefined) {
-      this.set('_isDisabled', value === true);
-    }
-    return value === true ? get(this, 'disabledClass') : false;
-  })
-});
-
 export default Ember.Component.extend({
   session: Ember.inject.service(),
   tagName: "nav",
-  classNames: ["navbar","navbar-full"],
-  notAuthed: Ember.computed('session.isAuthenticated', function() {
-    return !this.get('session.isAuthenticated');
-  }),
-  didRender() {
-    this.$('a').tooltip('dispose');
-    this.$('a.disabled').tooltip({
-      title: 'Please log in or sign up to use this section of the website',
-      placement: 'bottom',
-      trigger: 'hover'
-    });
-  }
+  classNames: ["navbar","navbar-full"]
 });
