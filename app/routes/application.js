f34cfb4ec52a17c0c0750abe62a3dc1903e9beea
jetpackpony <jetpackpony@gmail.com>
2016-12-22 22:50:56 +0300

    Skip transitions after login

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+const { getOwner } = Ember;
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  sessionAuthenticated() {
+    // do not redirect or anything
+  },
   actions: {
     error(error, transition) {
       if (error) {
