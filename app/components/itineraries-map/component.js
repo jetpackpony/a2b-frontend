2034a1add72b83a89dd5a4eb67171ab42a29cdcd
jetpackpony <jetpackpony@gmail.com>
2017-11-11 22:08:14 +0300

    Refactor itineraries-map component

--- app/components/itineraries-map/component.js
+++ app/components/itineraries-map/component.js
@@ -5,82 +5,60 @@ export default Ember.Component.extend({
   selectedItinerary: null,
   itineraries: Ember.A([]),
   bounds: Ember.computed('itineraries.[]', function() {
-    let bounds = Ember.A([]);
-    this.get('itineraries').forEach((iti) => {
-      iti.get('routes').map((route) => {
-        bounds.pushObject([route.get('fromLat'), route.get('fromLng')].map(parseFloat));
-        bounds.pushObject([route.get('toLat'), route.get('toLng')].map(parseFloat));
-      });
-    });
-    return bounds;
+    return Ember.A(
+      this.get('itineraries').reduce((bounds, iti) => bounds.concat(
+        iti.get('routes').reduce((bnds, route) => bnds.concat([
+          [route.get('fromLat'), route.get('fromLng')].map(parseFloat),
+          [route.get('toLat'), route.get('toLng')].map(parseFloat)
+        ]), [])
+      ), [])
+    );
   }),
   markers: Ember.computed('itineraries.[]', 'selectedItinerary', 'routeHovered', function() {
     return Ember.A(this.get('itineraries')
       .reduce((markers, iti) => markers.concat(iti.get('routes')
         .reduce((mrks, route) => mrks.concat([
-          {
-            id: `${route.get('id')}-from`,
-            coords: [route.get('fromLat'), route.get('fromLng')],
-            style: this._getLineStyle(iti.get('id'), route.get('id')),
-            title: route.get('fromAddress')
-          },
-          {
-            id: `${route.get('id')}-to`,
-            coords: [route.get('toLat'), route.get('toLng')],
-            style: this._getLineStyle(iti.get('id'), route.get('id')),
-            title: route.get('toAddress')
-          }
+          this._createMarker(iti, route, "from"),
+          this._createMarker(iti, route, "to"),
         ]), [])
       ), [])
     );
   }),
   lines: Ember.computed('itineraries.[]', 'selectedItinerary', 'routeHovered', function() {
-    let lines = Ember.A([]);
-    this.get('itineraries').forEach((iti) => {
-      iti.get('routes').forEach((route) => {
-        let line = {
+    return Ember.A(
+      this.get('itineraries').reduce((lines, iti) => lines.concat(
+        iti.get('routes').map((route) => ({
           id: route.get('id'),
           from: [route.get('fromLat'), route.get('fromLng')],
-          to: [route.get('toLat'), route.get('toLng')]
-        };
-
-        if (this._isRouteSelected(route.get('id'))) {
-          line.style = 'selected';
-        } else if (this._isItinerarySelected(iti.get('id'))) {
-          line.style = 'normal';
-        } else {
-          line.style = 'dimmed';
-        }
-        lines.pushObject(line);
-      });
-    });
-    return lines;
+          to: [route.get('toLat'), route.get('toLng')],
+          style: this._getLineStyle(iti.get('id'), route.get('id'))
+        }))
+      ), [])
+    );
   }),
   _getLineStyle(itiId, routeId) {
-    if (this._isRouteSelected(routeId)) {
-      return 'selected';
-    }
-    if (this._isItinerarySelected(itiId)) {
-      return 'normal';
-    }
+    if (this._isRouteSelected(routeId)) return 'selected';
+    if (this._isItinerarySelected(itiId)) return 'normal';
     return 'dimmed';
   },
   _isItinerarySelected(id) {
-    if (!this.get('selectedItinerary')) {
-      return false;
-    }
-    if (this.get('selectedItinerary').get('id') !== id) {
-      return false;
-    }
-    return true;
+    return !(
+      !this.get('selectedItinerary')
+      || this.get('selectedItinerary').get('id') !== id
+    );
   },
   _isRouteSelected(id) {
-    if (!this.get('routeHovered')) {
-      return false;
-    }
-    if (this.get('routeHovered').get('id') !== id) {
-      return false;
-    }
-    return true;
+    return !(
+      !this.get('routeHovered')
+      || this.get('routeHovered').get('id') !== id
+    );
+  },
+  _createMarker(itinerary, route, prefix) {
+    return {
+      id: `${route.get('id')}-${prefix}`,
+      coords: [route.get(`${prefix}Lat`), route.get(`${prefix}Lng`)],
+      style: this._getLineStyle(itinerary.get('id'), route.get('id')),
+      title: route.get(`${prefix}Address`)
+    };
   },
 });
