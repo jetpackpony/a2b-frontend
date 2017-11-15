95d0ce6f17ded7cf8451f1f9f4afd78e85045a06
jetpackpony <jetpackpony@gmail.com>
2017-11-15 21:24:11 +0300

    Refactor create-new-route-details component
      * remove registerChild and make form reset based on model values
      * extract a bunch of pure functions
      * cleanup template file

--- app/components/create-new-route-details/component.js
+++ app/components/create-new-route-details/component.js
@@ -1,60 +1,121 @@
 import Ember from 'ember';
+import Route from '../../models/route';
+
+const transportTypes = ["Bus", "Ferry", "Train"];
 
 export default Ember.Component.extend({
-  transportTypes: Ember.A(["Bus", "Ferry", "Train", "Other"]),
+  transportTypeFieldValues: Ember.A(transportTypes),
   newRoute: null,
-  showOtherTransportField: false,
-  showSubmitButton: Ember.computed('newRoute.{transportType,organization,duration,price,description,fromLat,fromLng,fromComment,toLat,toLng,toComment}', function() {
-    let route = this.get('newRoute');
-    if (route.get('transportType') && route.get('organization')
-        && route.get('duration') && route.get('price')
-        && route.get('description') && this._locationsSet(route)) {
-      return true;
-    }
-    return false;
-  }),
-  _locationsSet(route) {
-    if ((route.get('fromLat') && route.get('fromLat')) ||
-        route.get('fromComment')) {
-      if ((route.get('toLat') && route.get('toLat'))
-        || route.get('toComment')) {
-          return true;
-        }
-    }
-    return false;
-  },
   init() {
     this._super(...arguments);
-    this.get('registerChild')(this);
+    // Generate a computed property which watches all parameters
+    // of the model changes
+    Ember.defineProperty(
+      this,
+      'showSubmitButton',
+      Ember.computed(
+        `newRoute.{${getAttributeNames(Route).mapBy('name').join(',')}}`,
+        function() {
+          return isRouteComplete(this.get('newRoute'));
+        })
+    );
   },
-  reset() {
-    this.$('#transportType').val("");
-    this.$('#otherTransport').val("");
-    this.$('#duration-hours').val("");
-    this.$('#duration-minutes').val("");
-    this.set('showOtherTransportField', false);
-  },
+
+  showOtherTransportField: Ember.computed('newRoute.transportType', {
+    get() {
+      return !!this.get('newRoute.transportType') &&
+        transportTypes.indexOf(this.get('newRoute.transportType')) === -1;
+    },
+    set(key, value) {
+      return value;
+    }
+  }),
+  transportTypeEmpty: Ember.computed(
+    'newRoute.transportType',
+    'showOtherTransportField',
+    function() {
+      return !this.get('newRoute.transportType')
+        && !this.get('showOtherTransportField');
+    }
+  ),
+
+  durationHours: Ember.computed('newRoute.duration', {
+    get() {
+      return getHours(this.get('newRoute.duration'));
+    },
+    set(key, value) {
+      this.set('newRoute.duration',
+        addHours(this.get('newRoute.duration'), value));
+      return value;
+    }
+  }),
+  durationMinutes: Ember.computed('newRoute.duration', {
+    get() {
+      return getMinutes(this.get('newRoute.duration'));
+    },
+    set(key, value) {
+      this.set('newRoute.duration',
+        addMinutes(this.get('newRoute.duration'), value));
+      return value;
+    }
+  }),
+
   actions: {
     submit() {
       this.get('submit')();
     },
     transportTypeChanged() {
-      let val = event.target.value;
-      if (val === 'Other') {
-        this.set('showOtherTransportField', true);
-      } else {
-        this.set('showOtherTransportField', false);
-        this.set('newRoute.transportType', val);
-      }
-    },
-    durationChanged() {
-      let duration = parseInt(this.$('#duration-hours').val())
-        + parseInt(this.$('#duration-minutes').val() || "0") / 60;
-      this.set('newRoute.duration', duration.toString());
+      (event.target.value === 'Other')
+        ? (
+          this.set('newRoute.transportType', ""),
+          this.set('showOtherTransportField', true)
+        )
+        : this.set('newRoute.transportType', event.target.value);
     },
     back() {
       this.get('back')();
     }
   }
-
 });
+
+const getAttributeNames = (model) => {
+  let attrs = [];
+  Ember.get(model, 'attributes').forEach((name) => attrs.push(name));
+  return attrs;
+};
+
+const isRouteComplete = (route) => (
+  (route.get('transportType') && route.get('organization')
+    && route.get('duration') && route.get('price')
+    && route.get('description') && isRouteLocationsSet(route))
+);
+
+const isAddressAdded = (route, prefix) => (
+  (route.get(`${prefix}Lat`) && route.get(`${prefix}Lat`))
+  || route.get(`${prefix}Comment`)
+)
+
+const isRouteLocationsSet = (route) => (
+  isAddressAdded(route, 'from') && isAddressAdded(route, 'to')
+);
+
+const getHours = (time) => (
+  (time)
+  ? Math.floor(time)
+  : null
+);
+
+const getMinutes = (time) => (
+  (time)
+  ? Math.round((time % 1) * 60)
+  : null
+);
+
+const addHours = (time, hours) => (
+  ((time || 0) % 1) + parseInt(hours)
+);
+
+const addMinutes = (time, minutes) => (
+  Math.floor(time || 0) + parseInt(minutes || 0) / 60
+);
+
