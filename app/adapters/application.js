5bb2f1958c4558fec12cdba35a5090cea1a1007b
jetpackpony <jetpackpony@gmail.com>
2017-11-17 22:06:05 +0300

    Refactor application adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,11 +5,13 @@ import ENV from '../config/environment';
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:oauth2',
   host: ENV.a2b.apiEndPoint,
-  handleResponse: function(status, headers, payload, req){
-    if(payload.errors){
-      let msg = `Server error occured while processing: '${req.method} ${req.url}'`;
-      return new DS.AdapterError(payload.errors, msg);
-    }
-    return this._super(...arguments);
+  handleResponse: function(status, headers, payload, req) {
+    return (payload.errors)
+      ? new DS.AdapterError(payload.errors, getErrorMsg(req))
+      : this._super(...arguments);
   }
 });
+
+const getErrorMsg = (req) => (
+ `Server error occured while processing: '${req.method} ${req.url}'`
+);
