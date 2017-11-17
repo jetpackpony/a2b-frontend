45913d15448f5f22351530badc43c06c617c2bde
jetpackpony <jetpackpony@gmail.com>
2017-11-17 12:08:46 +0300

    Move highlighting selected itinerary to itinerary-list-item

--- app/components/itineraries-list/component.js
+++ app/components/itineraries-list/component.js
@@ -4,25 +4,14 @@ export default Ember.Component.extend({
   classNames: ['itineraries-list'],
 
   selectedItinerary:null,
-  didRender() {
-    if (this.get('selectedItinerary')) {
-      this.selectItinerary(this.get('selectedItinerary'));
-    }
-  },
+  openedItinerary: null,
   actions: {
     itemHovered(itinerary) {
-      if (itinerary !== this.get('selectedItinerary')) {
-        this.set('selectedItinerary', itinerary);
-        this.selectItinerary(itinerary);
-      }
+      this.set('selectedItinerary', itinerary);
     },
     itemClicked(itinerary) {
       this.set('selectedItinerary', itinerary);
       this.set('openedItinerary', itinerary);
     }
-  },
-  selectItinerary(itinerary) {
-    this.$('.itinerary-list-item').removeClass('highlighted');
-    this.$(`#itinerary-${itinerary.id}`).addClass('highlighted');
   }
 });
--- app/components/itinerary-list-item/component.js
+++ app/components/itinerary-list-item/component.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  itinerarySelected: Ember.computed('selectedItinerary', 'itinerary', function() {
+    return this.get('itinerary.id') === this.get('selectedItinerary.id');
+  })
 });
