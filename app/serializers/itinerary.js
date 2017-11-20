e1fc074e67dd2ed0714f452c606b584bb2614778
jetpackpony <jetpackpony@gmail.com>
2017-11-20 20:10:58 +0300

    Refactor itinerary serializer - make everything more functional

--- app/serializers/itinerary.js
+++ app/serializers/itinerary.js
@@ -1,25 +1,42 @@
+import R from 'npm:ramda';
 import DS from 'ember-data';
-
-const putRoutesInOrder = function(order, items) {
-  let res = order.map((routeId) => {
-    return items.find((route) => route.id === routeId);
-  });
-  return res;
-};
 
 export default DS.JSONAPISerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    // Sort routes inside itinerary in proper order
-    if (Array.isArray(payload.data)) {
-      payload.data = payload.data.map((item) => {
-        item.relationships.routes.data = putRoutesInOrder(
-          item.id.split('-'),
-          item.relationships.routes.data
-        );
-        return item;
-      });
-    }
-
-    return this._super(...arguments);
+    return this._super(
+      store,
+      primaryModelClass,
+      // Sort routes inside itinerary in proper order
+      R.merge(payload, {
+        data: sortRouteInItineraries(payload.data)
+      }),
+      id,
+      requestType
+    );
   }
 });
+
+const itineraryRoutesLens =
+  R.lensPath(['relationships', 'routes', 'data']);
+
+const sortRouteInItineraries = (data) => (
+  (Array.isArray(data))
+  ? data.map((itinerary) => (
+    R.over(
+      itineraryRoutesLens,
+      putRoutesInOrder(getRouteOrder(itinerary)),
+      itinerary
+    )
+  ))
+  : data
+);
+
+const getRouteOrder = (itinerary) => (
+  itinerary.id.split('-')
+);
+
+const putRoutesInOrder = R.curry((order, items) => (
+  order.map((routeId) => (
+    items.find((route) => route.id === routeId)
+  ))
+));
